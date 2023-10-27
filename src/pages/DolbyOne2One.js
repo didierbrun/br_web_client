f57a538fe9cc731fecf2be1b28d24f4851584bb9
Didier Brun <db@brundidier.com>
2023-10-27 12:15:02 +0200

    Add token as env property

--- src/pages/DolbyOne2One.js
+++ src/pages/DolbyOne2One.js
@@ -10,7 +10,8 @@ const DolbyOne2One = () => {
     const [connected, setConnected] = useState(false)
 
     const initializeToken = () => {
-        const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkb2xieS5pbyIsImlhdCI6MTY5ODMxMTIxNywic3ViIjoicUpZMXBMLUlDOWZqbEYwYzhMYmpZQT09IiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9DVVNUT01FUiJdLCJ0YXJnZXQiOiJzZXNzaW9uIiwib2lkIjoiZWQyYjY5MDctYWIwYS00YzA2LTk1ODItMDE4YzJmMjE0ZGMyIiwiYWlkIjoiMTYyMjQzYmUtYzMyZC00MWYzLTkyNTktMjNmYzkyYWY3OWM2IiwiYmlkIjoiOGEzNjhjMWY4NDYxYzAzYTAxODQ4NmFlNjYzMTY0NjUiLCJleHAiOjE2OTgzOTc2MTd9.vZ-vU0-tEIk3SVNmJuiQYX6dW03CztQlTECcoKwcEuP8p_U26cmMNWjJtzI8G9AwwyQ72rOnLB2qrawhB0v95w"
+        const token = process.env.REACT_APP_DOLBY_ACCESS_TOKEN
+        console.log(token)
         VoxeetSDK.initializeToken(token, () => new Promise((resolve) => resolve(token)));
         console.log("Web SDK initialized")
         return token;
